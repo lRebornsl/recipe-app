3cdec45720affe72bd5e1b0014f82a37f4c931c9
lRebornsl <anthony_vasquez98@hotmail.com>
2023-10-11 11:40:30 -0500

    Configure user devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  root "users#index"
-  devise_for :users
+	devise_for :users
+  
+  devise_scope :user do
+    authenticated :user do
+      root to: 'users#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+  
   resources :users do
     resources :recipes
   end
