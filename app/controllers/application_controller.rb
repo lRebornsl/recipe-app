aa3244c9e40acfe7ffffa250531069640b3b70d0
Nicolas Bolañez <Zven94>
2023-10-12 12:04:37 -0300

    updated rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :update_allowed_parameters, if: :devise_controller?
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :email, :password, :current_password)
-    end
-  end
-
-  def after_sign_out_path_for(_resource_or_scope)
-    new_user_session_path
-  end
-
-  private
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation, :current_password])
+  end
+
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
   end
 end
