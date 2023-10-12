b8d4f4e7427ec4a140d7b38eef8ec5ec66d96447
Anthony Vásquez <103215273+lRebornsl@users.noreply.github.com>
2023-10-12 09:35:31 -0500

    Revert "Pull-Resquest: Create Recipes"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   protected
 
   def update_allowed_parameters
@@ -16,11 +14,5 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
-  end
-
-  private
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+require "active_support/core_ext/integer/time"
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -78,6 +80,4 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
 end
