242e373bd6a2188b51df828c24018c4d6dfd2dd2
lRebornsl <anthony_vasquez98@hotmail.com>
2023-10-12 09:20:25 -0500

    Add food list

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,36 @@
 class FoodsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @foods = Food.where(user_id: @user.id)
+  end
+
+  def new
+    @food = Food.new
+    @user = current_user
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @user = current_user
+    @food.user_id = @user.id
+
+    if @food.save
+      redirect_to user_foods_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @food = Food.find_by(user_id: params[:user_id], id: params[:id])
+    @food&.destroy
+
+    redirect_to user_foods_path(current_user)
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+  end
 end
