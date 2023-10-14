75eee3779b5e76e8f641e4bf8e48aa848e8c059b
lRebornsl <anthony_vasquez98@hotmail.com>
2023-10-14 13:00:02 -0500

    Fix application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
   def calculate_total_price(recipe)
     total_price = 0
-    recipe.foods.each do |food|
-      total_price += food.price * food.quantity
+    recipe.recipe_foods.each do |recipe_food|
+      food = recipe_food.food
+      total_price += food.price * recipe_food.quantity
     end
     number_to_currency(total_price)
   end
