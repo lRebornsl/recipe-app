181d3366e029e0fa5bdec7575f6845d1893ebdda
lRebornsl <anthony_vasquez98@hotmail.com>
2023-10-15 00:40:55 -0500

    Apply changes asked

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
     end
   end
   
-  resources :public_recipes, only: [:index]
   resources :users do
     resources :foods, only: [:index, :new, :create, :destroy]
     resources :recipes do
@@ -22,4 +21,5 @@ Rails.application.routes.draw do
       resources :shopping_lists, only: [:index]
     end
   end
+  resources :public_recipes, only: [:index]
 end
